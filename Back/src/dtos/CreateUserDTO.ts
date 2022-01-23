3a09f0fc713e4b252e49de0a20287acf5c8c9cd4
Herdeson Mourão <herdesondev@gmail.com>
2022-01-23 01:36:25 -0300

    Feat :sparkles: implementando arquivo de dto dos users

--- Back/src/dtos/CreateUserDTO.ts
+++ Back/src/dtos/CreateUserDTO.ts
@@ -1,8 +1,19 @@
+import * as yup from 'yup';
+
 export interface CreateUserDTO {
   name: string;
   username: string;
   password: string;
   email: string;
   whatsapp: string;
-  avatar_logo: string;
-}
+  avatar_logo?: string;
+};
+
+export const createUserRequestSchema = yup.object({
+  name: yup.string().required(),
+  username: yup.string().required(),
+  password: yup.string().required(),
+  email: yup.string().required().email('Email e um campo obrigatorio!'),
+  whatsapp: yup.string().required(),
+  avatar_logo: yup.string()
+});
