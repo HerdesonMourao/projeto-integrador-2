bc0ef4ba775de595f7c278191e239f86c6ae57d7
Herdeson Mourão <herdesondev@gmail.com>
2022-02-05 08:37:14 -0300

    Feat :sparkles: removendo migration antigas

--- Back/prisma/migrations/20220131030007_add_is_activated_card/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "cards" ADD COLUMN     "is_activated" BOOLEAN NOT NULL DEFAULT true;
--- Back/prisma/migrations/20220204221256_add_is_activated_revenue/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "revenue" ADD COLUMN     "is_activated" BOOLEAN NOT NULL DEFAULT true;
