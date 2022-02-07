69c09b4271d7f65ff0e816b5197a9b08d1dbe395
Herdeson Mourão <herdesondev@gmail.com>
2022-02-06 23:23:57 -0300

    Feat :sparkles: implementando services das entidades

---
+++ Front/src/app/service/expenditure.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Expenditure } from '../models/Expenditure';
+import { ApiService } from './api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExpenditureService {
+
+  constructor(
+    private api: ApiService
+  ) { }
+
+  store(expenditure: Expenditure): Observable<Expenditure> {
+    return this.api.post('expenditure', expenditure);
+  }
+
+  index(): Observable<Expenditure[]> {
+    return this.api.get('expenditure');
+  }
+
+  show(id: number): Observable<Expenditure> {
+    return this.api.get('expenditure/' + `${id}`);
+  }
+
+  update(expenditure: Expenditure, id: any): Observable<any> {
+    return this.api.put(`expenditure/${id}`, expenditure);
+  }
+
+  destroy(id: number): Observable<any> {
+    return this.api.delete('expenditure/', `${id}`);
+  }
+}
